01aeafb5f12c32c0c1faeafcaefcdc28fc1b3e3f
just-erp.a <alvin.l@just-erp.com>
2025-07-07 23:54:35 +0800

    [Initial Commit]

--- odoo_module/uber_marketplace/models/__init__.py
+++ odoo_module/uber_marketplace/models/__init__.py
@@ -2,3 +2,5 @@ from . import ubereats_config
 from . import ubereats_token
 from . import res_config_settings
 from . import pos_config
+from . import ubereats_store
+from . import ubereats_api
--- odoo_module/uber_marketplace/wizard/__init__.py
+++ odoo_module/uber_marketplace/wizard/__init__.py
@@ -1,1 +1,2 @@
 from . import ubereats_auth_wizard
+from . import ubereats_store_discovery_wizard
